6a24e884c792daaa990d472e94e1c61d4a592531
Emirhan <emohanasd@gmail.com>
2025-04-09 05:14:40 +0300

    VeriTabanıListeleme
    Basit bir C# ile veri tabanını kullandım

---
+++ Form1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace OtobusKartApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.chkOgrenci = new System.Windows.Forms.CheckBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.dgvKartlar = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKartlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(127, 57);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(181, 22);
+            this.txtIsim.TabIndex = 0;
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(451, 57);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(145, 22);
+            this.txtSoyisim.TabIndex = 1;
+            // 
+            // chkOgrenci
+            // 
+            this.chkOgrenci.AutoSize = true;
+            this.chkOgrenci.Location = new System.Drawing.Point(465, 125);
+            this.chkOgrenci.Name = "chkOgrenci";
+            this.chkOgrenci.Size = new System.Drawing.Size(76, 20);
+            this.chkOgrenci.TabIndex = 3;
+            this.chkOgrenci.Text = "Öğrenci";
+            this.chkOgrenci.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(142, 125);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(802, 121);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 5;
+            this.btnListele.Text = "LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // dgvKartlar
+            // 
+            this.dgvKartlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKartlar.Location = new System.Drawing.Point(142, 164);
+            this.dgvKartlar.Name = "dgvKartlar";
+            this.dgvKartlar.RowHeadersWidth = 51;
+            this.dgvKartlar.RowTemplate.Height = 24;
+            this.dgvKartlar.Size = new System.Drawing.Size(689, 399);
+            this.dgvKartlar.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "İSİM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(497, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "SOYİSİM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(799, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "DOĞUM TARİHİ";
+            // 
+            // dtpDogumTarihi
+            // 
+            this.dtpDogumTarihi.Location = new System.Drawing.Point(747, 57);
+            this.dtpDogumTarihi.Name = "dtpDogumTarihi";
+            this.dtpDogumTarihi.Size = new System.Drawing.Size(200, 22);
+            this.dtpDogumTarihi.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 575);
+            this.Controls.Add(this.dtpDogumTarihi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvKartlar);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.chkOgrenci);
+            this.Controls.Add(this.txtSoyisim);
+            this.Controls.Add(this.txtIsim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKartlar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.CheckBox chkOgrenci;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.DataGridView dgvKartlar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDogumTarihi;
+    }
+}
+
---
+++ Form1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace OtobusKartApp
+{
+    public partial class Form1 : Form
+    {
+        // ✅ Düzgün bağlanıp bağlanmadığını kontrol etmek için bağlantı dizesini tekrar tanımla
+        private string connectionString = @"Server=SERMET\SQLEXPRESS;Database=OtobusKartDB;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ✅ Veriyi veritabanına ekleyen metod
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+
+                    string query = "INSERT INTO OtobusKart (Isim, Soyisim, DogumTarihi, Ogrenci) VALUES (@Isim, @Soyisim, @DogumTarihi, @Ogrenci)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        // Kullanıcı girişlerini al
+                        string isim = txtIsim.Text;
+                        string soyisim = txtSoyisim.Text;
+                        DateTime dogumTarihi = dtpDogumTarihi.Value; // DateTimePicker kontrolünden tarih al
+                        bool ogrenciMi = chkOgrenci.Checked; // CheckBox kontrolünden öğrenci olup olmadığını al
+
+                        // Parametreleri ekle
+                        cmd.Parameters.AddWithValue("@Isim", isim);
+                        cmd.Parameters.AddWithValue("@Soyisim", soyisim);
+                        cmd.Parameters.AddWithValue("@DogumTarihi", dogumTarihi.ToString("yyyy-MM-dd"));
+                        cmd.Parameters.AddWithValue("@Ogrenci", ogrenciMi);
+
+                        int result = cmd.ExecuteNonQuery();
+
+                        if (result > 0)
+                        {
+                            MessageBox.Show("✅ Veri başarıyla eklendi!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("❌ Veri eklenemedi!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu: " + ex.Message);
+            }
+        }
+
+
+        // ✅ Veriyi listeleyen metod
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    MessageBox.Show("Bağlantı başarılı!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Bağlantı hatası: " + ex.Message);
+                }
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM OtobusKart";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvKartlar.DataSource = dt;
+            }
+        }
+
+    }
+}
